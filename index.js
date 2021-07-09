13c97f7e76e9de660ce7b4419905c855c3899687
Jobayer-Majumder <jobayermojumder70@gmail.com>
2021-07-09 18:59:42 +0600

    dotenv file added

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require('express')
 const app = express();
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+require('dotenv').config({ path: './.env' })
 
 app.use(cors());
 app.use(express.json());
 
+const PORT = 5000;
 
 
-const uri = "mongodb+srv://callingApi:callingApi@jobayer.eggfq.mongodb.net/callingAPi?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASS}@jobayer.eggfq.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const apiCollection = client.db("callingApi").collection("productApi");
+  const apiCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION}`);
   
   app.get('/getProduct', (req, res) => {
       apiCollection.find({})
@@ -28,4 +30,4 @@ app.get('/', (req, res) => {
     res.send('server running')
 })
 
-app.listen(5000, () => console.log('server is running on'))
+app.listen(PORT || process.env.PORT, () => console.log('server is running on'))
